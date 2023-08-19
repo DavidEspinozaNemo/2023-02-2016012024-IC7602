8dadcde9a961c3d69ed014a1c7919141caa218fe
root <root@Redes.myguest.virtualbox.org>
2023-08-19 15:42:07 -0600

    MOdificando las recestas para probar el chef repo

--- Proyecto0/chef-repo-public/cookbooks/test/recipes/recipe_pbx.rb
+++ Proyecto0/chef-repo-public/cookbooks/test/recipes/recipe_pbx.rb
@@ -1,29 +1,16 @@
-# Receta para la instalación y configuración de Asterisk
+# Receta para la descarga y configuración de FreePBX
 
-execute 'download_asterisk' do
-
-  command cd /home/redes/2023-02-2016012024-IC7602/Proyecto0/chef-repo-public/asterisk-18.19.0
-  command ldconfig
-
+execute 'download_freepbx' do
+  command 'cd freepbx'
+  command 'apt-get install nodejs npm -y'
+  command './install -n'
 end
 
-execute 'asterisk_permissions' do
-
-  command groupadd asterisk # Se crea un usuario y grupo dedicado
-  command useradd -r -d /var/lib/asterisk -g asterisk asterisk
-  command usermod -aG audio,dialout asterisk # Se da audio al grupo
-  command chown -R asterisk.asterisk /etc/asterisk # Se configuran los permisos
-  command chown -R asterisk.asterisk /var/{lib,log,spool}/asterisk
-  command chown -R asterisk.asterisk /usr/lib/asterisk
-
-end
-
-execute 'error_fix' do
-
-  command systemctl status asterisk
-  command sed -i 's";\[radius\]"\[radius\]"g' /etc/asterisk/cdr.conf
-  command sed -i 's";radiuscfg => /usr/local/etc/radiusclient-ng/radiusclient.conf"radiuscfg => /etc/radcli/radiusclient.conf"g' /etc/asterisk/cdr.conf
-  command sed -i 's";radiuscfg => /usr/local/etc/radiusclient-ng/radiusclient.conf"radiuscfg => /etc/radcli/radiusclient.conf"g' /etc/asterisk/cel.conf
-  command systemctl restart asterisk
-
+execute 'configure_freepbx' do
+  command 'sed -i \'s/^\(User\|Group\).*/\1 asterisk/\' /etc/apache2/apache2.conf # Se define el usuario de Asterisk dentro de apache'
+  command 'sed -i \'s/AllowOverride None/AllowOverride All/\' /etc/apache2/apache2.conf'
+  command 'sed -i \'s/\(^upload_max_filesize = \).*/\120M/\' /etc/php/5.6/apache2/php.ini # Se incrementa el tamaño máximo de php.ini'
+  command 'sed -i \'s/\(^upload_max_filesize = \).*/\120M/\' /etc/php/5.6/cli/php.ini'
+  command 'a2enmod rewrite'
+  command 'systemctl restart apache2'
 end
