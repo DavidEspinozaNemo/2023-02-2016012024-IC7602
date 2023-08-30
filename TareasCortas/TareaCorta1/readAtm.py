3c1803c324cb622eac42a1a3653778555bf7b927
Fao2212 <ferolsen@hotmail.com>
2023-08-29 18:17:40 -0600

    Se crea modifica el script para que tambien pueda escribir archivos.

--- TareasCortas/TareaCorta1/readAtm.py
+++ TareasCortas/TareaCorta1/readAtm.py
@@ -1,15 +1,36 @@
 initSection = "InitSection"
 endSection = "EndSection"
-lineDivision = "\n"
+fileExtension = ".atm"
+elementDivision = "\n"
 
 audio = []
 fourierAudio = []
+
+#Puedo mezclar los dos datos para solo hacer un recorrido. Asumiendo que ambos arrays son del mismo tamano.
+
+def guardarArchivo(audioArray,fourierAudioArray,path,fileName):
+    fileData = ""
+    #Audio section in file
+    fileData += initSection+elementDivision
+    for element in audioArray:
+        fileData += element+elementDivision
+    fileData += endSection+elementDivision
+    #Fourier section in file
+    fileData += initSection+elementDivision
+    for element in fourierAudioArray:
+        fileData += element+elementDivision
+    fileData += endSection+elementDivision
+
+    with open(path+fileName+fileExtension, 'r') as file:
+        file.write(fileData)
+        file.close()
+
     
 def processFileLines(lines):
     dataSections = []
     isSection = False
     for line in lines:
-        line = line.replace(lineDivision,"")
+        line = line.replace(elementDivision,"")
         if line == endSection:
             dataSections.append(dataSection)
             isSection = False
