b11e540477fc0ed5fb8ae0726d29fe5b8a2a0c4d
Fao2212 <ferolsen@hotmail.com>
2023-09-07 14:23:48 -0600

    Cambios de atm/ intalacion en venv y gitignore
    Se agrega al atm que solo lea los files con un path sin tener que especificarlo en el archivo para aprovechar la GUI.
    Se instala pyaudio en el venv
    Se modifica el gitignore para tomar en cuenta sistemas operativos VSC y python.

--- TareasCortas/TareaCorta1/atm.py
+++ TareasCortas/TareaCorta1/atm.py
@@ -3,31 +3,33 @@ import shutil
 import os
 import zipfile
 
-folderPathATM = "TareasCortas/TareaCorta1/ATMFiles"
-folderPathWAV = "TareasCortas/TareaCorta1/WavFiles"
-audioFormat = ".wav"
+#folderPathATM = "TareasCortas/TareaCorta1/ATMFiles"
 fileFormat = ".atm"
 compressionFormat = "zip"
-jsonFileName = 'data.json'
+jsonFileName = 'dataATM.json'
 
-#Tirar excepcion si no se carga bien
-def save(audioData,fourierData,audioFrecuency,audioFileName):
-
-    #Create new folder
-    atmPath = folderPathATM+"/"+audioFileName
-    if os.path.exists(atmPath):
-        shutil.rmtree(atmPath, ignore_errors=True)
-    os.makedirs(atmPath)
-
-    #Move audio to new path
-    shutil.move(folderPathWAV+"/"+audioFileName+audioFormat, atmPath)
-
+'''
+DATA EXAMPLE
     #Save audio data in a json
     data = {
         "audioData" : audioData,
         "fourierData" : fourierData,
         "audioFrecuency" : audioFrecuency
     }
+'''
+
+#Tirar excepcion si no se carga bien
+def save(data,audioSourcePath,ATMSavePath,audioFileName):
+
+    #Create new folder
+    atmPath = ATMSavePath+"/"+audioFileName
+    if os.path.exists(atmPath):
+        shutil.rmtree(atmPath, ignore_errors=True)
+    os.makedirs(atmPath)
+
+    #Move audio to new path
+    shutil.move(audioSourcePath+"/"+audioFileName, atmPath)
+
     with open(atmPath+'/'+jsonFileName, 'w') as file:
         json.dump(data, file, ensure_ascii=False, indent=4)
 
@@ -38,11 +40,10 @@ def save(audioData,fourierData,audioFrecuency,audioFileName):
         shutil.rmtree(atmPath, ignore_errors=True)
     print(f"Created {fileFormat} file: {atmPath}{fileFormat}")
 
-def load(audioFileName):
+def load(filePath):
     #Change name to zip to unzip file.
-    atmPath = folderPathATM+"/"+audioFileName
-    zipPath = atmPath+"."+compressionFormat
-    os.rename(atmPath+fileFormat , zipPath)
+    zipPath = filePath.replace(fileFormat,"."+compressionFormat)
+    os.rename(filePath , zipPath)
 
     #Open zip file and json file
     with zipfile.ZipFile(zipPath, "r") as file:
@@ -50,8 +51,8 @@ def load(audioFileName):
             utf = jsonFile.read()  
             data = json.loads(utf) 
     
-    os.rename(zipPath,atmPath+fileFormat)
-    print(f"Loaded {fileFormat} file: {atmPath}{fileFormat}\n data: {data}")
+    os.rename(zipPath,filePath)
+    print(f"Loaded {fileFormat} file: {filePath}\n data: {data}")
     return data
 
 #Save para crear un nuevo archivo.
