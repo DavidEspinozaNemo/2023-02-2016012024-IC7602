bbe8026b9a56b446426895d7844cccb690757a2a
Fao2212 <ferolsen@hotmail.com>
2023-10-08 15:45:53 -0600

    Chef

---
+++ proyecto1/squid-chef-repo/cookbooks/squid/libraries/helpers.rb
@@ -0,0 +1,60 @@
+module ChefSquidHelpers
+  # load them databags.
+  def squid_load_host_acl(databag_name)
+    host_acl = []
+    begin
+      data_bag(databag_name).each do |bag|
+        group = data_bag_item(databag_name, bag)
+        next unless group['net'].respond_to?(:each)
+        group['net'].each do |host|
+          host_acl.push [group['id'], group['type'], host]
+        end
+      end
+    rescue
+      Chef::Log.info "no '#{databag_name}' data bag"
+    end
+    host_acl
+  end
+
+  def squid_load_url_acl(databag_name)
+    url_acl = []
+    begin
+      data_bag(databag_name).each do |bag|
+        group = data_bag_item(databag_name, bag)
+        next unless group['urls'].respond_to?(:each)
+        group['urls'].each do |url|
+          url_acl.push [group['id'], group.key?('element') ? group['element'] : node['squid']['acl_element'], url]
+        end
+      end
+    rescue
+      Chef::Log.info "no '#{databag_name}' data bag"
+    end
+    url_acl
+  end
+
+  def squid_load_acls(databag_name)
+    acls = []
+    begin
+      data_bag(databag_name).each do |bag|
+        group = data_bag_item(databag_name, bag)
+        next unless group['acl'].respond_to?(:each)
+        group['acl'].each do |acl|
+          # An exclamation mark can be optionally included in the additional array element to invert the ACL
+          acls.push [acl[1], "#{acl[2] || ''}#{group['id']}", acl[0]]
+        end
+      end
+    rescue
+      Chef::Log.info "no '#{databag_name}' data bag"
+    end
+    acls
+  end
+
+  def squid_version_detected
+    command = %(#{node['squid']['package']} -v | grep Version | sed 's/.*Version \\\(.\\..\\\).*/\\1/g' | tr -d '\n')
+    command_out = shell_out(command)
+    command_out.stdout.to_f
+  end
+end
+
+Chef::Resource.include ChefSquidHelpers
+Chef::DSL::Recipe.include ChefSquidHelpers
---
+++ proyecto1/squid-chef-repo/cookbooks/squid/metadata.rb
@@ -0,0 +1,13 @@
+name              'squid'
+maintainer        'Sous Chefs'
+maintainer_email  'help@sous-chefs.org'
+license           'Apache-2.0'
+description       'Installs/configures squid as a simple caching proxy'
+chef_version      '>= 13'
+source_url        'https://github.com/sous-chefs/squid'
+issues_url        'https://github.com/sous-chefs/squid/issues'
+version           '5.1.3'
+
+%w(debian ubuntu centos redhat scientific suse amazon freebsd).each do |os|
+  supports os
+end
